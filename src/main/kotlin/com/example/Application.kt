84c2e8c0440abf3466c1a161a798f47cc07e5903
Sergio <garciasergiomar@gmail.com>
2021-10-11 19:26:20 -0500

    added logging to api calls

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,15 @@
 package com.example
 
+import com.example.plugins.configureGraphQL
+import com.example.plugins.configureLogging
+import com.example.plugins.configureSerialization
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureGraphQL()
         configureSerialization()
+        configureLogging()
     }.start(wait = true)
 }
