52b250e9ea69d17abeeb7eded36a02d615fa697b
Sergio <garciasergiomar@gmail.com>
2021-10-11 19:22:34 -0500

    added influxdb client

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val kgraphql_version: String by project
+val influxdb_client_version: String by project
 
 plugins {
     application
@@ -25,6 +26,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("com.apurebase:kgraphql:$kgraphql_version")
     implementation("com.apurebase:kgraphql-ktor:$kgraphql_version")
+    implementation("com.influxdb:influxdb-client-java:$influxdb_client_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
--- src/main/kotlin/com/example/graphql/queries/unitQueries.kt
+++ src/main/kotlin/com/example/graphql/queries/unitQueries.kt
@@ -2,9 +2,6 @@ package com.example.graphql.queries
 
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
 import com.example.graphql.MBUnit
-import com.example.graphql.Position
-import com.example.graphql.Town
-import java.util.*
 
 fun SchemaBuilder.unitQueries () {
     query("units") { // get lis of all available units
