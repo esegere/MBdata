bdf521720391961ca7c4c283aa6d151ee036e750
Sergio <garciasergiomar@gmail.com>
2021-10-14 14:09:08 -0500

    connected to database

--- src/main/kotlin/com/example/plugins/PostgreSQL.kt
+++ src/main/kotlin/com/example/plugins/PostgreSQL.kt
@@ -6,20 +6,21 @@ import org.jetbrains.exposed.sql.Database
 
 fun initDB() {
     val DATABASE: String = "mbdata"
-    val PORT: Int = 5432
-    val HOST: String = "localhost"
+    val PORT: Int = 54321
+    val HOST: String = "database"
 
     val config = HikariConfig().apply {
         driverClassName = "org.postgresql.Driver"
         jdbcUrl = "jdbc:postgresql://$HOST:$PORT/$DATABASE"
-        username = "esege"
-        password = "hirake"
+        username = "ktor"
+        password = "secret"
         maximumPoolSize = 3
         isAutoCommit = false
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
     }.also { config ->
         config.validate()
     }
+    print("connected")
 //    config.schema = <dbSchema>
     val ds = HikariDataSource(config)
     Database.connect(ds)
